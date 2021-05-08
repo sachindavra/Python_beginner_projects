8a8941d8630e93a629552288f82438a0e25a5801
sachindavra <raju.davra@gmail.com>
2021-05-08 23:59:43 +0530

    mad_libs_generator
    Signed-off-by: sachindavra <raju.davra@gmail.com>

---
+++ mad_libs/mad_libs.py
@@ -0,0 +1,7 @@
+color = input("Enter any color of your choice: ")
+CN = input("Enter any celebrity name: ")
+car = input("Which car do you have: ")
+
+print("I don't have fan which is " + color + " in color.")
+print("My favorite celebrity is " + CN)
+print("I have just bought " + car)
