1403957ad5cc15ea030bba978a64232421a111d1
sachindavra <raju.davra@gmail.com>
2021-05-13 11:56:08 +0530

    text_formatting
    Signed-off-by: sachindavra <raju.davra@gmail.com>

---
+++ text_formatting/text_formatting.py
@@ -0,0 +1,9 @@
+def print_formatting(n):
+    width = len("{0:b}".format(n))
+    for i in range(1, n+1):
+        print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
+
+
+n = int(input("Enter any number: "))
+print_formatting(n)
+
